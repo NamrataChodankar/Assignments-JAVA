019a341078dcbc0f76f126d3f759c9929c9e9972
NamrataChodankar <89849734+NamrataChodankar@users.noreply.github.com>
2021-09-27 01:51:28 +0530

    Add files via upload

---
+++ Assignments_17_09_21/ConvertToUpperCase.java
@@ -0,0 +1,45 @@
+package Assignment_17_09_21;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConvertToUpperCase {
+		
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			List<String> stringList = new ArrayList<>();
+			System.out.println("Enter 5 Names : ");
+			for (int i = 0; i < 5; i++) {
+				System.out.print("Enter Name "+(i+1)+ " : ");
+				stringList.add(sc.nextLine());
+			}
+			System.out.println("\nBefore Manipulation......");
+			System.out.println(stringList);
+			System.out.print("\nEnter index number that you want to convert to uppercase : ");
+			int index = sc.nextInt();
+			convertCharToupper(index-1, stringList);
+			System.out.println("\nAfter Manipulation.......");
+			System.out.println(stringList);
+		}
+	}
+	
+	
+	
+	public static void convertCharToupper(int index,List<String> list) {
+		Iterator<String> itr=list.iterator();
+		List<String> tempList = new ArrayList<>();
+		while(itr.hasNext()) {
+			String str = itr.next();
+			if(str.length()<index+1) {
+				continue;
+			}
+			char[] charArray = str.toCharArray();
+			charArray[index] = Character.toUpperCase(charArray[index]);
+			tempList.add(new String(charArray));
+			itr.remove();
+		}
+		list.addAll(tempList);
+	}
+}
